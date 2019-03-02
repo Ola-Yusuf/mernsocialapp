ba130c00fbf3fcc2433fa1c25f516a7b7fa286cc
Ola-Yusuf <olatundeyusuf90@gmail.com>
2019-03-02 09:12:08 +0100

    Inital express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURL:
+    "mongodb://hizy:olabisi@cluster0-shard-00-00-vdexl.mongodb.net:27017,cluster0-shard-00-01-vdexl.mongodb.net:27017,cluster0-shard-00-02-vdexl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+};
